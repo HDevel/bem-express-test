b264ead784adc38554e53c8fcd1f740a7a7f79ce
H.Devel <h.devel@gmail.com>
2016-09-26 10:23:24 +0300

    Скидки меньше 500р меня не интересуют

--- server/get-data.js
+++ server/get-data.js
@@ -25,7 +25,7 @@ module.exports = function(req, callback) {
                 hour = min * 60,
                 day = hour * 24;
 
-            diff = -0.001;
+            find['price.sale.price'] = { $lte: -500 };
             find['price.sale.date'] = { $gt: (new Date().getTime() - day) };
             sort = { "sort": [['price.date', -1]] };
         }
