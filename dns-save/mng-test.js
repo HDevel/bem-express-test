186f611dfdc900ad4d185bd20267a67af46e4e74
H.Devel <h.devel@gmail.com>
2016-11-30 14:14:00 +0300

    Профайлинг базы

--- dns-save/mng-test.js
+++ dns-save/mng-test.js
@@ -7,9 +7,23 @@ MongoClient.connect(mongoProps.path, function(err, db) {
     db.authenticate(mongoProps.user, mongoProps.password, function(err, res) {
         var collection = db.collection('items');
 
-        collection.find({ name: /Ноутбук/ }).toArray(function(err, docs) {
-            console.log(docs && docs[0]);
-            process.exit();
-        });
+        perf(10);
+
+        function perf(count) {
+            var profile = new Date();
+
+            collection.find({ code: 1039653 }).toArray(function(err, docs) {
+                // console.log(docs && docs[0]);
+                console.log(profile - new Date());
+
+                if (count > 0) {
+                    perf(--count);
+                } else {
+                    db.close();
+                    process.exit();
+                }
+            });
+        }
     });
 });
+
